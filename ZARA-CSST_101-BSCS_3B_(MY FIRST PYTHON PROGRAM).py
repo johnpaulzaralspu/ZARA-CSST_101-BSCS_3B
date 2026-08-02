8cc1fa7d6730b613d07b634d1f9261e20e09a25f
John Paul Carrillo Zara <johnpaul.zara@lspu.edu.ph>
2026-08-02 21:32:54 +0800

    Add files via upload

---
+++ ZARA-CSST_101-BSCS_3B_(MY FIRST PYTHON PROGRAM).py
@@ -0,0 +1,8 @@
+# Labaratory Activity 3: My first Python Program
+
+print("\nName: John Paul C. Zara")
+print("Course: BS Computer Science")
+print("Section: BSCS-3B")
+print("Favorite AI Technology: Machine Learning, LLM's and AI Agents")
+print("Future AI Project: AI Data Quality Assesment using LLM's and AI Agents or AI Chatbot")
+
